0e5fa2f5aa378b458a1894d8422fb916cb208537
taiyaki373 <2410008@i-seifu.jp>
2025-10-22 09:38:27 +0900

    no message

--- j1015/Matango.java
+++ j1015/Matango.java
@@ -3,8 +3,4 @@ package j1015;
 public class Matango {
     int hp;
     final int LEVEL=10;
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 6a07b3f5ac2dbd4e1cbaca94d6cb630390bef4c3
--- j1015/Mian.java
+++ j1015/Mian.java
@@ -11,8 +11,4 @@ public class Mian {
         matango.hp=50;
         System.out.println("マタンゴのHPは"+matango.hp);
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 6a07b3f5ac2dbd4e1cbaca94d6cb630390bef4c3
